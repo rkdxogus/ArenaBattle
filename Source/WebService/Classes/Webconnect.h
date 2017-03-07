6d157b80d52fc301445f4493ecbb7f7f767df341
rkdxogus <rkdxogus7000@gmail.com>
2017-03-07 15:52:18 +0900

    WebServiceStart
    WebServiceStart

---
+++ Source/WebService/Classes/Webconnect.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "UObject/NoExportTypes.h"
+#include "Webconnect.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WEBSERVICE_API UWebconnect : public UObject
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
